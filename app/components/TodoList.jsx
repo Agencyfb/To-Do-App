9477b7ccf0a9f72ba3024481eb998d94b9d6a274
agencyfb <alaliphotographics@gmail.com>
2026-04-30 15:47:44 +0100

    chane a word

--- app/components/TodoList.jsx
+++ app/components/TodoList.jsx
@@ -51,7 +51,7 @@ const TodoList = () => {
 
         <input 
           type="text"
-          placeholder="Describe"
+          placeholder="Description"
           value={description}
           onChange={(e) => setDescription(e.target.value)}
           className="w-full mb-3 px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
